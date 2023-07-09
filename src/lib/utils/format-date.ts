d77d8a613cb7c80a666210f41f3757ea038c4448
Maciej Kubus <maciek.krol.zycia@gmail.com>
2023-07-09 22:42:04 +0200

    + format date util

---
+++ src/lib/utils/format-date.ts
@@ -0,0 +1,26 @@
+export const formatDate = (date: string, format: string): string => {
+  const dateObj = new Date(date);
+  
+  const day = dateObj.getDate();
+  const month = dateObj.getMonth() + 1;
+  const year = dateObj.getFullYear();
+  const hour = dateObj.getHours();
+  const minute = dateObj.getMinutes();
+  const second = dateObj.getSeconds();
+
+  const dayStr = day < 10 ? `0${day}` : day;
+  const monthStr = month < 10 ? `0${month}` : month;
+  const hourStr = hour < 10 ? `0${hour}` : hour;
+  const minuteStr = minute < 10 ? `0${minute}` : minute;
+  const secondStr = second < 10 ? `0${second}` : second;
+
+  let formatStr = format;
+  formatStr = formatStr.replace('DD', '' + dayStr);
+  formatStr = formatStr.replace('MM', '' + monthStr);
+  formatStr = formatStr.replace('YYYY', '' + year);
+  formatStr = formatStr.replace('HH', '' + hourStr);
+  formatStr = formatStr.replace('mm', '' + minuteStr);
+  formatStr = formatStr.replace('ss', '' + secondStr);
+
+  return formatStr;
+}
