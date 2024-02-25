e701a821ee6797feee041114ef06f456347f349c
Maciej Kubus <maciek.krol.zycia@gmail.com>
2024-02-25 15:59:31 +0100

    file grid 5 cols

--- src/lib/components/file-grid/file-grid.svelte
+++ src/lib/components/file-grid/file-grid.svelte
@@ -132,7 +132,7 @@
 			Selected: {checkedRows.length}
 		</div>
 	{/if}
-	<div class="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-2">
+	<div class="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-2">
 		{#each files as row, i}
 			<button
 				class="rounded-lg border-4 group relative
