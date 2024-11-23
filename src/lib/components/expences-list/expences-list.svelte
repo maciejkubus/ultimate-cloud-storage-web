da23a9f15a4b4429f13e7b2e32a88a09c4c82576
Maciej Kubus <maciek.krol.zycia@gmail.com>
2024-11-23 17:04:18 +0100

    + style

--- src/lib/components/expences-list/expences-list.svelte
+++ src/lib/components/expences-list/expences-list.svelte
@@ -33,22 +33,25 @@
 <div class="w-full variant-filled-surface rounded-lg overflow-hidden">
 	{#each expences as expence, index}
 		<div
-			class="w-full flex flex-col xl:flex-row px-8 py-2 xl:gap-8 xl:justify-center xl:items-center
+			class="w-full flex flex-col xl:flex-row px-8 py-4 xl:gap-8 xl:justify-center xl:items-center
               {index % 2 == 1 ? 'bg-surface-700' : ''}
-              {index == 0 ? 'pt-4' : ''}
+              {index == 0 ? 'pt-8' : ''}
               {index == expences.length - 1 ? 'pb-4' : ''}
       "
 		>
-			<div class="text-xs xl:text-base">
-				{formatDate(expence.created ? expence.created : '')}
+			<div class="flex flex-col xl:gap-2 xl:justify-center xl:items-center">
+				<div class="xl:text-2xl font-bold">{expence.amount} zł</div>
+				<div class="text-xs xl:text-base">
+					{formatDate(expence.created ? expence.created : '')}
+				</div>
 			</div>
-			<div class="flex flex-col gap-1 xl:gap-4 w-full">
+			<div class="flex flex-col gap-1 xl:gap-2 w-full pt-4 xl:pt-0">
 				<h4 class="text-lg xl:text-xl font-bold">{expence.name}</h4>
 				{#if expence.description}
 					<p class="text-sm xl:text-base">{expence.description}</p>
 				{/if}
 			</div>
-			<div class="">
+			<div class="flex justify-end pt-4 xl:pt-0">
 				<button
 					on:click|stopPropagation|preventDefault={() => openModalRemove(expence)}
 					class="text-primary-600 hover:text-primary-500"
